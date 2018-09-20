a7cac96c1a1ede18bae52729269eb841f26886f7
AtalPandey <atal.pandey@target.com>
2018-09-20 14:53:03 +0530

    initial import

---
+++ src/main/java/com/demo/helloworld/HelloworldApplication.java
@@ -0,0 +1,18 @@
+package com.demo.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class HelloworldApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(HelloworldApplication.class, args);
+    }
+}
